cc61b9887595532fd98287a32f47fe2f6641921e
Sagitario25 <adanielsanchezgarcia@gmail.com>
2021-04-16 04:10:31 +0200

    Based in local appdata, more commands

--- file_mng.py
+++ file_mng.py
@@ -1,30 +1,52 @@
 import os
-import zipfile
+import shutil
+
+def createPath (path):
+	dir = os.path.basename (os.path.normpath (path))
+	root = path [:-len (dir)]
+	if os.path.exists (root):
+		os.mkdir (path)
+	else:
+		createPath (root)
 
 class Manager:
-	def __init__ (self, archivePath):
-		self.archivePath = archivePath
-		if not os.path.exists (self.archivePath):
-			zipfile.ZipFile (self.archivePath, "w", zipfile.ZIP_DEFLATED).close ()
+	def __init__ (self, path):
+		self.path = path
+		if not os.path.exists (self.path):
+			createPath (self.path)
 
-	def add (self, file):
-		with zipfile.ZipFile (self.archivePath, "a", zipfile.ZIP_DEFLATED) as zip:
-			if not file [1] == ":":#If the path isnt absolute
-				raise Exception ("Priveded path must be absolute")
-			if len (file.split ('/')) > 1:
-				zip.write (file, file.split ('/')[-1])
-			else:
-				zip.write (file, file.split ('\\')[-1])
-	def get (self, file, path):
-		with zipfile.ZipFile (self.archivePath, "r", zipfile.ZIP_DEFLATED) as zip:
-			zip.extract (file, path)
-	
-	def reset (self):
-		zipfile.ZipFile (self.archivePath, "w", zipfile.ZIP_DEFLATED).close ()
+	def add (self, src):
+		"""Adds to archive"""
+		if os.path.isfile (src):
+			shutil.copy (src, self.path)
+		else:
+			shutil.copytree (src, os.path.join (self.path, os.path.basename (src)))
 
+	def get (self, name, outpath):
+		"""Exports files and dirs to the desired path"""
+		if os.path.isfile (os.path.join (self.path, name)):
+			shutil.copy (os.path.join (self.path, name), outpath)
+		else:
+			shutil.copytree (os.path.join (self.path, name), os.path.join (outpath, name))
 
+	def rm (self, name):
+		"""Removes from archive"""
+		if os.path.isfile (os.path.join (self.path, name)):
+			os.remove (os.path.join (self.path, name))
+		else:
+			shutil.rmtree (os.path.join (self.path, name))
+
+	def kidnap (self, src):
+		"""Adds to archive and deletes original version"""
+		self.add (src)
+		if os.path.isfile (src):
+			os.remove (src)
+		else:
+			shutil.rmtree (src)
+
+	def getout (self, name, outpath):
+		"""Gets put and deletes archive version"""
+		self.get (name, outpath)
+		self.rm (name)
 if __name__ == "__main__":
-	mng = Manager ("C:/Users/Admin/Desktop/test/stupid.dll")
-	mng.add ("C:/Users/Admin/Desktop/Honeygain.exe")
-	mng.get ("Honeygain.exe", "C:/Users/Admin/Desktop/test")
-	mng.remove ()
+	mng = Manager (os.path.join (os.getenv ("localappdata"), "escudoweb"))
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import file_mng
 import command as cmnd
 import os
 
-mng = file_mng.Manager ("Firebase.Auth.dll")
+mng = file_mng.Manager (os.path.join (os.getenv ("localappdata"), "escudoweb"))
 
 command = cmnd.Interpreter ()
 command.addCommand ("add", mng.add)
 command.addCommand ("get", mng.get)
-command.addCommand ("reset", mng.reset)
+command.addCommand ("rm", mng.rm)
+command.addCommand ("kidnap", mng.kidnap)
+command.addCommand ("getout", mng.getout)
 
 if __name__ == "__main__":
 	args = sys.argv
